906704d372c8639327164dae722052c6d02da849
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-05-19 20:14:50 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles/styles.scss";
+
+
+function component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = "Hello!";
+
+  return element;
+}
+
+document.body.appendChild(component());
